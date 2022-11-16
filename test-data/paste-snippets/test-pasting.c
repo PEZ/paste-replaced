f43235874d8f74f101f34936b68670f5164f7f7d
Peter Strömberg <pez@pezius.com>
2022-11-16 17:04:42 +0100

    Create test-data project

---
+++ test-data/paste-snippets/test-pasting.c
@@ -0,0 +1,15 @@
+
+
+<div class="foo bar">
+    baz
+</div>
+
+
+; Normal paste breaks the string
+""
+
+; Paste with the quoting raplacer
+""
+
+; Use the quoting replacer via the custom keyboard shortcut
+""
