53e077fac8c125e5c3af956ed2f331eaa6148132
Peter Strömberg <pez@pezius.com>
2022-11-26 14:05:45 +0100

    some test-data

---
+++ test-data/replacer-test.c
@@ -0,0 +1,17 @@
+
+
+<div class="foo bar">
+    baz
+</div>
+
+
+; Normal paste breaks the string
+"<div class="foo bar">
+    baz
+</div>"
+
+; Paste with the quoting raplacer
+"<div class=\"foo bar\">\n baz\n</div>"
+
+; Use the quoting replacer via the custom keyboard shortcut
+"<div class=\"foo bar\">\n baz\n</div>"
