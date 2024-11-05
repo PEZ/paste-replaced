83654b2ce066b628ee8f5d6859d7c6b43ed4b7af
Peter Strömberg <pez@pezius.com>
2024-11-05 19:06:35 +0100

    Use `testing` macro

--- test/paste_replaced/pauses_test.cljs
+++ test/paste_replaced/pauses_test.cljs
@@ -3,9 +3,10 @@
             [paste-replaced.pauses :as sut]))
 
 (deftest pause
-  (is (= 75 (sut/pause "foo" :intermediate sut/typing-pauses)))
-  (is (= 250 (sut/pause " foo" :intermediate sut/typing-pauses)))
-  (is (= 250 (sut/pause "  foo" :intermediate sut/typing-pauses)))
-  (is (= 250 (sut/pause "foo\t" :intermediate sut/typing-pauses)))
-  (is (= 1300 (sut/pause "\nfoo" :intermediate sut/typing-pauses)))
-  (is (= 1300 (sut/pause "foo  bar" :intermediate sut/typing-pauses))))
+  (testing "It identifies the correct pause from the input"
+    (is (= 75 (sut/pause "foo" :intermediate sut/typing-pauses)))
+    (is (= 250 (sut/pause " foo" :intermediate sut/typing-pauses)))
+    (is (= 250 (sut/pause "  foo" :intermediate sut/typing-pauses)))
+    (is (= 250 (sut/pause "foo\t" :intermediate sut/typing-pauses)))
+    (is (= 1300 (sut/pause "\nfoo" :intermediate sut/typing-pauses)))
+    (is (= 1300 (sut/pause "foo  bar" :intermediate sut/typing-pauses)))))
