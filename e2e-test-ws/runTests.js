ecd7c66f68589dcbdd414ca7fd038ac2ca21d8bc
Peter Strömberg <pez@pezius.com>
2024-11-06 01:33:57 +0100

    Don't try to activate Calva =)

--- e2e-test-ws/runTests.js
+++ e2e-test-ws/runTests.js
@@ -1,7 +1,6 @@
 const vscode = require('vscode');
 
 exports.run = async () => {
-  await vscode.commands.executeCommand('calva.activateCalva');
   return vscode.commands.executeCommand(
     'joyride.runCode',
     "(require '[test-runner :as runner]) (runner/run-all-tests)"
