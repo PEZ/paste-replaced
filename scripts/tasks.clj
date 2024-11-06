f9374628d8d003b57692697491fab3f5758cfa92
PEZ <>
2024-11-06 22:31:16 +0100

    Fix bug with creating bumped pre-release

--- scripts/tasks.clj
+++ scripts/tasks.clj
@@ -2,8 +2,7 @@
   (:require [babashka.process :as p]
             [clojure.string :as string]
             publish
-            util
-            [babashka.cli :as cli]))
+            util))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn publish! [args]
@@ -15,18 +14,20 @@
     (println changelog-text)))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn bump-version! [{:keys [user-email user-name dry]}]
-  (println "Bumping version")
-  (util/sh dry "git" "config" "--global" "user.email" user-email)
-  (util/sh dry "git" "config" "--global" "user.name" user-name)
-  (util/sh dry "npm" "set" "git-tag-version" "false")
-  (util/sh dry "npm" "version" "patch")
-  (util/sh dry "git" "add" ".")
-  (let [version (-> (util/sh false "node" "-p" "require('./package').version")
-                    :out
-                    string/trim)]
-    (util/sh dry "git" "commit" "-m" (str "Bring on version " version "!")))
-  (util/sh dry "git" "push" "origin" "HEAD"))
+(defn bump-version! [{:keys [user-email user-name dry force]}]
+  (if force
+    (do
+      (println "Bumping version")
+      (util/shell dry "git" "config" "--global" "user.email" user-email)
+      (util/shell dry "git" "config" "--global" "user.name" user-name)
+      (util/shell dry "npm" "version" "--no-git-tag-version" "patch")
+      (util/shell dry "git" "add" ".")
+      (let [version (-> (util/sh false "node" "-p" "require('./package').version")
+                        :out
+                        string/trim)]
+        (util/shell dry "git" "commit" "-m" (str "Bring on version " version "!")))
+      (util/shell dry "git" "push" "origin" "HEAD"))
+    (println "Use --force to actually bump the version")))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn package-pre-release! [{:keys [branch dry]}]
@@ -36,20 +37,20 @@
                       :out string/trim)
         random-slug (util/random-slug 2)
         slugged-branch (string/replace branch #"/" "-")
-        pre-id (str slugged-branch "-" commit-id "-" random-slug)]
+        version (str current-version "-" slugged-branch "-" commit-id "-" random-slug)]
     (println "Current version:" current-version)
     (println "HEAD Commit ID:" commit-id)
     (println "Packaging pre-release...")
-    (println (:out (util/sh dry "npm" "version" "--no-git-tag-version" "prerelease" "--preid" pre-id)))
-    (println (:out (util/sh dry "npx" "vsce" "package" "--pre-release")))
-    (println (:out (util/sh dry "npm" "version" "--no-git-tag-version" current-version)))))
+    (util/shell dry "npm" "version" version)
+    (util/shell dry "npx" "vsce" "package" "--pre-release")
+    (util/shell dry "npm" "version" "--no-git-tag-version" current-version)))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn run-e2e-tests-with-vsix! [{:keys [vsix]}]
   (println "Running end-to-end tests using vsix:" vsix)
-  (p/shell "node" "./e2e-test-ws/launch.js" (str "--vsix=" vsix)))
+  (util/shell false "node" "./e2e-test-ws/launch.js" (str "--vsix=" vsix)))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn run-e2e-tests-from-working-dir! []
   (println "Running end-to-end tests using working directory")
-  (p/shell "node" "./e2e-test-ws/launch.js"))
+  (util/shell false "node" "./e2e-test-ws/launch.js"))
--- scripts/util.clj
+++ scripts/util.clj
@@ -18,6 +18,15 @@
       (throw-if-error
        (apply p/sh args)))))
 
+(defn shell [dry-run? & args]
+  (if dry-run?
+    (do (println "Dry run:" (apply pr-str args))
+        {:exit 0})
+    (do
+      (apply println args)
+      (flush)
+      (apply p/shell args))))
+
 (defn random-slug [n]
   (let [chars "abcdefghijklmnopqrstuvwxyz"]
     (apply str (repeatedly n #(rand-nth chars)))))
