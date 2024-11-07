a1f702f8f94809d6da702426b8bc26ded5207577
PEZ <>
2024-11-07 10:45:12 +0100

    WIP: discover tests

--- e2e-test-ws/.joyride/src/test_runner/runner.cljs
+++ e2e-test-ws/.joyride/src/test_runner/runner.cljs
@@ -3,7 +3,8 @@
             [cljs.test]
             [promesa.core :as p]
             [test-runner.config :as config]
-            [test-runner.db :as db]))
+            [test-runner.db :as db]
+            ["vscode" :as vscode]))
 
 (defn- write [& xs]
   (js/process.stdout.write (string/join " " xs)))
@@ -51,7 +52,52 @@
         (p/reject! running fail-reason)
         (p/resolve! running true)))))
 
-(defn run-all-tests []
+(defn test-file? [file]
+  (and (.endsWith file ".cljs")
+       (not (.startsWith file "_"))))
+
+(defn file->ns [file]
+  (-> file
+      (string/replace #"/" ".")
+      (string/replace #"\.cljs$" "")
+      (string/replace #"^tests\." "")))
+
+(defn find-test-files+ [dir]
+  (p/let [js-dir-entries (vscode/workspace.fs.readDirectory dir)
+          dir-entries (js->clj js-dir-entries)
+          test-files (p/all (keep (fn [[name type]]
+                                    (let [full-path (vscode/Uri.joinPath dir name)]
+                                      (if (= type vscode/FileType.Directory)
+                                        (find-test-files+ full-path)
+                                        (when (and (= type vscode/FileType.File)
+                                                   (string/ends-with? name "_test.cljs"))
+                                          (p/resolved full-path)))))
+                                  dir-entries))]
+    (flatten test-files)))
+
+(defn get-test-namespaces+ [tests-directory]
+  (p/let [files (find-test-files+ tests-directory)]
+    (def files files)
+    (->> files
+         (filter #(test-file? (first %)))
+         (map #(file->ns (first %)))
+         (map symbol))))
+
+(comment
+  (def tests-directory (vscode/Uri.joinPath
+                        (-> vscode/workspace.workspaceFolders
+                            first
+                            .-uri)
+                        "e2e-test-ws"
+                        ".joyride"
+                        "src"))
+  (p/let [test-namespaces (get-test-namespaces+ tests-directory)]
+    (println "test-namespaces" test-namespaces)
+    (def test-namespaces test-namespaces)
+    :rcf)
+  )
+
+(defn run-all-tests [tests-directory]
   (let [running (p/deferred)]
     (swap! db/!state assoc :running running)
     (try
---
+++ e2e-test-ws/.joyride/src/tests/foo/bar/b_test.cljs
@@ -0,0 +1,1 @@
+(ns tests.foo.bar.b-test)
