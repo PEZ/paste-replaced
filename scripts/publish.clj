5c117cbdbf440eaa61438b2bbbfe5fadc452c33e
PEZ <pez@pezius.com>
2024-11-18 09:41:22 +0100

    Update publish script from extension template

--- scripts/publish.clj
+++ scripts/publish.clj
@@ -53,25 +53,15 @@
   (println "Pushing")
   (util/shell dry-run? "git" "push" "--follow-tags"))
 
-(defn git-status []
-  (println "Checking git status")
-  (let [git-status (util/shell false "git" "status")
-        [_ branch] (re-find #"^On branch (\S+)\n" git-status)
-        up-to-date (re-find #"Your branch is up to date" git-status)
-        clean (re-find #"nothing to commit, working tree clean" git-status)]
-    (cond-> #{}
-      (not= "master" branch) (conj :not-on-master)
-      (not up-to-date) (conj :not-up-to-date)
-      (not clean) (conj :branch-not-clean))))
-
 (defn tag-and-push! [version dry-run?]
   (tag version dry-run?)
   (push dry-run?)
   (println "Open to follow the progress of the release:")
-  (println "  https://github.com/PEZ/paste-replaced/actions"))
+  (println "  https://github.com/PEZ/vsc-et/actions"))
 
-(defn publish! [unreleased-changelog-text dry-run?]
-  (let [changelog-text (slurp changelog-filename)
+(defn publish! [dry-run?]
+  (let [unreleased-changelog-text (get-changelog-text-for-version "Unreleased")
+        changelog-text (slurp changelog-filename)
         extension-version (-> (slurp "package.json")
                               json/parse-string
                               (get "version"))]
@@ -89,22 +79,37 @@
                           dry-run?)
         (tag-and-push! extension-version dry-run?)))))
 
-(defn yolo! [{:keys [dry]}]
-  (let [unreleased-changelog-text (get-changelog-text-for-version "Unreleased")
+(defn- get-git-situation []
+  (println "Checking git status")
+  (let [git-status (:out (util/sh false "git" "status"))
+        [_ branch] (re-find #"^On branch (\S+)\n" git-status)
+        up-to-date (re-find #"Your branch is up to date" git-status)
+        clean (re-find #"nothing to commit, working tree clean" git-status)]
+    (cond-> #{}
+      (not= "master" branch) (conj :not-on-master)
+      (not up-to-date) (conj :not-up-to-date)
+      (not clean) (conj :branch-not-clean))))
 
-        status (git-status)]
-    (println "dry-run?" dry)
-    (if (or (seq status)
-            (empty? unreleased-changelog-text))
+(defn- collect-situation []
+  (let [unreleased-changelog-text (get-changelog-text-for-version "Unreleased")
+        git-situation (get-git-situation)]
+    (if (seq unreleased-changelog-text)
       (do
-        (when (seq status)
-          (println "Git status issues: " status))
-        (when (empty? unreleased-changelog-text)
-          (println "There are no unreleased changes in the changelog."))
+        (println "Unreleased changelog entry:\n" unreleased-changelog-text)
+        git-situation)
+      (do (println "There are no unreleased changes in the changelog.")
+          (conj git-situation :no-unreleased-changelog)))))
+
+(defn yolo! [{:keys [dry]}]
+  (println "dry-run?" dry)
+  (let [situation (collect-situation)]
+    (if (seq situation)
+      (do
+        (println "These issues with publishing were found:" situation)
         (println "Release anyway? YES/NO: ")
         (flush)
         (let [answer (str (read))]
           (if-not (= "YES" answer)
             (println "Aborting publish.")
-            (publish! unreleased-changelog-text dry))))
-      (publish! unreleased-changelog-text dry))))
+            (publish! dry))))
+      (publish! dry))))
